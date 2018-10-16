2ec3705e7b4dd410cd331369fe348be9394a70c4
Josh Harris <josh.harris212@yahoo.co.uk>
2018-10-16 13:15:05 +0000

    Updated further

--- lib/app.rb
+++ lib/app.rb
@@ -1,6 +1,23 @@
 require 'sinatra'
+set :session_secret, 'super secret'
+
+get '/' do
+    'hello'
+end
+
+get '/secret' do
+    'This is a secret page'
+end
+
+
 
 get '/dog' do
-    name = ["Oswald", "Peter", "Clive"].sample
+    @name = ["Oswald", "Peter", "Clive"].sample
     erb(:index)
+end
+
+get '/named-dog' do
+    p params
+    @name = params[:name]
+    erb :index
 end
